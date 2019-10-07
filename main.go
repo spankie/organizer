7ccf3aadadbef775624fa97ab3be0cb07f2f3684
Odohi David <odohi.david@gmail.com>
2019-10-07 20:07:19 +0100

    add flags to determine running migrations and also intialize the database

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/spankie/organizer/controllers"
+	"github.com/spankie/organizer/models"
 	_ "github.com/spankie/organizer/routers"
 
 	"github.com/astaxie/beego"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+	migrate := flag.Bool("migrate", false, "Indicate if migrations should be run.")
+	flag.Parse()
+	db, err := models.GetDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if *migrate {
+		log.Println("Migrations run already...")
+		models.RunMigrations()
+		return
+	}
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
-	beego.Router("/user", &controllers.UserController{})
+
+	beego.Router("/api/user", &controllers.UserController{})
 	beego.Run()
 }
