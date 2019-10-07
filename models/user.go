09fe57de4d5bf32fbd59e8d77c85a92177e0fb89
Odohi David <odohi.david@gmail.com>
2019-10-07 20:12:11 +0100

    refactor user model to use the database

--- models/user.go
+++ models/user.go
@@ -2,8 +2,14 @@ package models
 
 import (
 	"errors"
+	"log"
 	"strconv"
 	"time"
+
+	"github.com/astaxie/beego"
+	"github.com/jinzhu/gorm"
+	"github.com/pusher/jwt-go"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -12,35 +18,51 @@ var (
 
 func init() {
 	UserList = make(map[string]*User)
-	u := User{"user_11111", "astaxie", "11111", Profile{"male", 20, "Singapore", "astaxie@gmail.com"}}
+	u := User{
+		Password: "password",
+		Gender:   "male",
+		FullName: "Spankie Dee",
+		Age:      20,
+		Address:  "Singapore",
+		Email:    "spankie@gmail.com",
+	}
 	UserList["user_11111"] = &u
 }
 
 type User struct {
-	Id       string
-	Username string
-	Password string
-	Profile  Profile
+	gorm.Model
+	Password       string       `json:"password"`
+	Gender         string       `json:"gender"`
+	FullName       string       `json:"full_name"`
+	Age            int          `json:"age"`
+	Address        string       `json:"address"`
+	Email          string       `json:"email"`
+	OrganizationID uint         `json:"organization_id"`
+	Organization   Organization `json:"organization" gorm:"foreignkey:OrganizationID"`
 }
 
-type Profile struct {
-	Gender  string
-	Age     int
-	Address string
-	Email   string
-}
-
-func AddUser(u User) string {
-	u.Id = "user_" + strconv.FormatInt(time.Now().UnixNano(), 10)
-	UserList[u.Id] = &u
-	return u.Id
+func AddUser(u User) []error {
+	err := DB.Create(&u).GetErrors()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+	// u.Id = strconv.FormatInt(time.Now().UnixNano(), 10)
+	// UserList[u.Id] = &u
+	// return u.Id
 }
 
 func GetUser(uid string) (u *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		return u, nil
+	u = &User{}
+	if err := DB.Where("id = ?", uid).First(u).Error; err != nil {
+		return nil, err
 	}
-	return nil, errors.New("User not exists")
+	return u, nil
+	// if u, ok := UserList[uid]; ok {
+	// return u, nil
+	// }
+	// return nil, errors.New("User not exists")
 }
 
 func GetAllUsers() map[string]*User {
@@ -49,36 +71,62 @@ func GetAllUsers() map[string]*User {
 
 func UpdateUser(uid string, uu *User) (a *User, err error) {
 	if u, ok := UserList[uid]; ok {
-		if uu.Username != "" {
-			u.Username = uu.Username
+		if uu.Email != "" {
+			u.Email = uu.Email
 		}
 		if uu.Password != "" {
 			u.Password = uu.Password
 		}
-		if uu.Profile.Age != 0 {
-			u.Profile.Age = uu.Profile.Age
-		}
-		if uu.Profile.Address != "" {
-			u.Profile.Address = uu.Profile.Address
-		}
-		if uu.Profile.Gender != "" {
-			u.Profile.Gender = uu.Profile.Gender
-		}
-		if uu.Profile.Email != "" {
-			u.Profile.Email = uu.Profile.Email
-		}
+		// if uu.Profile.Age != 0 {
+		// u.Profile.Age = uu.Profile.Age
+		// }
+		// if uu.Profile.Address != "" {
+		// u.Profile.Address = uu.Profile.Address
+		// }
+		// if uu.Profile.Gender != "" {
+		// u.Profile.Gender = uu.Profile.Gender
+		// }
+		// if uu.Profile.Email != "" {
+		// u.Profile.Email = uu.Profile.Email
+		// }
 		return u, nil
 	}
 	return nil, errors.New("User Not Exist")
 }
 
-func Login(username, password string) bool {
-	for _, u := range UserList {
-		if u.Username == username && u.Password == password {
-			return true
-		}
+func Login(username, password string) (string, []error) {
+	u := &User{}
+	errs := DB.Where("email = ?", username).First(u).GetErrors()
+	if len(errs) > 0 {
+		log.Println(errs)
+		return "", errs
 	}
-	return false
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil {
+		log.Println(err)
+		return "", []error{err}
+	}
+
+	// generate token for the user
+	claims := struct {
+		Email string `json:"email"`
+		jwt.StandardClaims
+	}{
+		Email: u.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(7 * 24 * time.Hour).Unix(),
+			Issuer:    "api.org.it",
+			Id:        strconv.Itoa(int(u.ID)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signingKey := []byte(beego.AppConfig.String("JWT_SECRET"))
+	tokenString, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", []error{err}
+	}
+
+	return tokenString, nil
 }
 
 func DeleteUser(uid string) {
