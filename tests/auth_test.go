fbe726f093a6fe2c4d37fbf671ff224c4dc33f49
Odohi David <odohi.david@gmail.com>
2019-10-09 06:11:06 +0100

    add authentication test

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -16,6 +16,10 @@ import (
 	_ "github.com/spankie/organizer/routers"
 )
 
+// func init() {
+
+// }
+
 func doesBodyContain(b *bytes.Buffer, expected string) bool {
 	bodyBytes, err := ioutil.ReadAll(b)
 	if err != nil {
@@ -31,62 +35,90 @@ func shouldContainInBody(actual interface{}, expected ...interface{}) string {
 	return fmt.Sprintf("Body does not contain %s", expected[0])
 }
 
-// TestGet is a sample to run an endpoint test
-func TestLogin(t *testing.T) {
-	u, err := json.Marshal(models.User{Username: "spankie@gmail.com", Password: "password"})
+func Register() (*httptest.ResponseRecorder, error) {
+	u, err := json.Marshal(models.User{
+		Password: "password",
+		FullName: "Spankie Dee",
+		Age:      30,
+		Address:  "Ajah, Lagos, Nigeria",
+		Email:    "spankie@gmail.com",
+		Gender:   "male",
+	})
+	if err != nil {
+		return nil, err
+	}
+	r, _ := http.NewRequest("POST", "/v1/user", bytes.NewBuffer(u))
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+	return w, nil
+}
+
+// TestRegister is a sample to run an endpoint test
+func TestRegister(t *testing.T) {
+	db, err := models.GetDB()
+	Convey("GetDB(Register)", t, func() {
+		Convey("DB should not return an error", func() {
+			So(err, ShouldBeNil)
+		})
+	})
+	defer db.Close()
+	w, err := Register()
 	if err != nil {
 		t.Fatal(err)
 	}
-	r, _ := http.NewRequest("GET", "/v1/user/login", bytes.NewBuffer(u))
+
+	beego.Trace(fmt.Sprintf("testing Register Code[%d]\n%s", w.Code, w.Body.String()))
+
+	Convey("Subject: Test Register Endpoint\n", t, func() {
+		Convey("Status Code Should Be 201", func() {
+			So(w.Code, ShouldEqual, http.StatusCreated)
+		})
+		Convey("The Result Should Not Be Empty", func() {
+			So(w.Body.Len(), ShouldBeGreaterThan, 0)
+		})
+		Convey("The result should contain message response: `success`", func() {
+			So(w.Body, shouldContainInBody, "successful")
+		})
+	})
+}
+
+// TestLogin is a sample to run an endpoint test
+func TestLogin(t *testing.T) {
+	db, err := models.GetDB()
+	Convey("GetDB(Login)", t, func() {
+		Convey("DB should not return an error", func() {
+			So(err, ShouldBeNil)
+		})
+	})
+	defer db.Close()
+	rr, err := Register()
+	if err != nil {
+		t.Fatal(err)
+	}
+	Convey("Subject: Test Register on login\n", t, func() {
+		Convey("Status Code Should Be 201", func() {
+			So(rr.Code, ShouldEqual, http.StatusCreated)
+		})
+	})
+	u, err := json.Marshal(models.User{Email: "spankie@gmail.com", Password: "password"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := http.NewRequest("POST", "/v1/user/login", bytes.NewBuffer(u))
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
 
 	beego.Trace("testing Login Code [%d]\n%s", w.Code, w.Body.String())
 
-	Convey("Subject: Test Station Endpoint\n", t, func() {
+	Convey("Subject: Test Login Endpoint\n", t, func() {
 		Convey("Status Code Should Be 200", func() {
 			So(w.Code, ShouldEqual, 200)
 		})
 		Convey("The Result Should Not Be Empty", func() {
 			So(w.Body.Len(), ShouldBeGreaterThan, 0)
 		})
-		Convey("The result should contain message response", func() {
-			So(w.Body, shouldContainInBody, "success")
-		})
-	})
-}
-
-// TestGet is a sample to run an endpoint test
-func TestRegister(t *testing.T) {
-	u, err := json.Marshal(models.User{
-		Username: "spankie@gmail.com",
-		Password: "password",
-		Profile: models.Profile{
-			FullName: "Spankie Dee",
-			Age:      30,
-			Address:  "Ajah, Lagos, Nigeria",
-			Email:    "spankie@gmail.com",
-			Gender:   "male",
-		},
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	r, _ := http.NewRequest("POST", "/v1/user", bytes.NewBuffer(u))
-	w := httptest.NewRecorder()
-	beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-	beego.Trace("testing Register Code[%d]\n%s", w.Code, w.Body.String())
-
-	Convey("Subject: Test Station Endpoint\n", t, func() {
-		Convey("Status Code Should Be 200", func() {
-			So(w.Code, ShouldEqual, 200)
-		})
-		Convey("The Result Should Not Be Empty", func() {
-			So(w.Body.Len(), ShouldBeGreaterThan, 0)
-		})
-		Convey("The result should contain message response", func() {
-			So(w.Body, shouldContainInBody, "successful")
+		Convey("The result should contain token", func() {
+			So(w.Body, shouldContainInBody, "token")
 		})
 	})
 }
