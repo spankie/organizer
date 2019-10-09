24783441d3c016f650fd22348a83ba2544c1c623
Odohi David <odohi.david@gmail.com>
2019-10-09 06:10:53 +0100

    swagger documentation

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -52,6 +52,51 @@ func init() {
             Filters: nil,
             Params: nil})
 
+    beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"] = append(beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"] = append(beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"] = append(beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"] = append(beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"] = append(beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:OrganizationController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
     beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/spankie/organizer/controllers:UserController"],
         beego.ControllerComments{
             Method: "Post",
@@ -101,7 +146,7 @@ func init() {
         beego.ControllerComments{
             Method: "Login",
             Router: `/login`,
-            AllowHTTPMethods: []string{"get"},
+            AllowHTTPMethods: []string{"post"},
             MethodParams: param.Make(),
             Filters: nil,
             Params: nil})
