a3e05ae8b3613cc95f7a23845443a490ca236b83
Odohi David <odohi.david@gmail.com>
2019-10-13 14:58:59 +0100

    refactor the dashboard nav

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
-import { Layout, Menu, Breadcrumb, Icon, Avatar } from "antd";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import avatar from "../assets/images/logo.svg";
 
 import AddUser from "../components/adduser/AddUser";
 import ListUsers from "../components/listusers/Listuser";
@@ -10,6 +11,7 @@ import "./Dashboard.css";
 import styled from "styled-components";
 
 const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu, Item } = Menu;
 
 function Dashboard() {
   const [state, setState] = useState({ collapsed: false });
@@ -19,59 +21,96 @@ function Dashboard() {
     setState({ collapsed });
   };
 
-  const AvatarImg = styled.section`
-    float: right;
-    padding-right: 30px;
-    display: flex;
-    flex-direction: column;
-    margin-top: 7px;
-    align-items: center;
+  // const AvatarImg = styled.section`
+  //   float: right;
+  //   padding-right: 30px;
+  //   display: flex;
+  //   flex-direction: column;
+  //   margin-top: 7px;
+  //   align-items: center;
 
-    &:hover {
-      ul {
-        z-index: 9999;
-        list-style-type: none;
-        align-items: flex-start;
-        display: flex;
-        margin: 0px 0px 0px 0px !important;
-        padding: 0px 0px 0px 0px;
+  //   &:hover {
+  //     ul {
+  //       z-index: 9999;
+  //       list-style-type: none;
+  //       align-items: flex-start;
+  //       display: flex;
+  //       margin: 0px 0px 0px 0px !important;
+  //       padding: 0px 0px 0px 0px;
 
-        border-radius: 5px;
-        border: 1px solid #ebedf0;
-        background-color: #cccccc;
+  //       border-radius: 5px;
+  //       border: 1px solid #ebedf0;
+  //       background-color: #cccccc;
 
-        li {
-          padding-left: 10px;
-          padding-right: 10px;
-        }
-      }
-    }
-  `;
+  //       li {
+  //         padding-left: 10px;
+  //         padding-right: 10px;
+  //       }
+  //     }
+  //   }
+  // `;
+
+  let UserTitle = (
+    <div>
+        <span
+          style={{
+            marginLeft: 0,
+            width: "30px",
+            height: "30px",
+            display: "inline-block",
+          }}
+        >
+          <img
+            src={avatar}
+            width="100%"
+            height="100%"
+            alt="img"
+          />
+        </span>
+        <span>Dee</span>
+      </div>
+  )
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider collapsible collapsed={state.collapsed} onCollapse={onCollapse}>
+      <Sider
+        collapsible
+        collapsed={state.collapsed}
+        onCollapse={onCollapse}
+        collapsedWidth="0"
+        breakpoint="sm"
+      >
         <div className="logo" />
         <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
           <Menu.Item key="1">
-            <Icon type="home" />
+            <div>
+              <Icon type="home" />
               <Link to="/dashboard/">Organization</Link>
+            </div>
           </Menu.Item>
           <Menu.Item key="2">
-            <Icon type="user-add" />
+            <div>
+              <Icon type="user-add" />
               <Link to="/dashboard/adduser">Add User</Link>
+            </div>
           </Menu.Item>
           <Menu.Item key="3">
-            <Icon type="usergroup-add" />
+            <div>
+              <Icon type="usergroup-add" />
               <Link to="/dashboard/listuser">List User</Link>
+            </div>
           </Menu.Item>
           <Menu.Item key="4">
-            <Icon type="setting" />
+            <div>
+              <Icon type="setting" />
               <Link to="/dashboard/settings">Change Password</Link>
+            </div>
           </Menu.Item>
           <Menu.Item key="5">
-            <Icon type="poweroff" />
-            <span>Logout</span>
+            <div>
+              <Icon type="poweroff" />
+              <span>Logout</span>
+            </div>
           </Menu.Item>
         </Menu>
       </Sider>
@@ -80,20 +119,31 @@ function Dashboard() {
           style={{ background: "#fff", padding: 0 }}
           className="header-avatar"
         >
-          <AvatarImg>
-            <Avatar size={50} icon="user"></Avatar>
-            <ul>
-              <li>
-                <Icon type="poweroff" />
-              </li>
-              <li>Logout</li>
-            </ul>
-          </AvatarImg>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            // className={style.user}
+            defaultSelectedKeys={["user"]}
+            style={{ lineHeight: "64px" }}
+          >
+            <SubMenu
+              title={UserTitle}
+              style={{
+                float: "right",
+                fontSize: "14px",
+                borderLeft: "1px solid fade(#e9e9e9, 5) !important",
+              }}
+              key="user">
+              <Item key="a">Profile</Item>
+              <Item key="b">Settings</Item>
+              <Item key="c">Logout</Item>
+            </SubMenu>
+          </Menu>
         </Header>
         <Content style={{ margin: "0 16px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}></Breadcrumb>
 
-          <div style={{ padding: 24, background: "#fff", minHeight: '100%' }}>
+          <div style={{ padding: 24, background: "#fff", minHeight: "100%" }}>
             <Route path="/dashboard/adduser" component={AddUser} />
             <Route path="/dashboard/listuser" component={ListUsers} />
             <Route path="/dashboard/settings" component={ChangePassword} />
