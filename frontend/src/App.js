c572c127e278509475725fef404099af73f96d17
Odohi David <odohi.david@gmail.com>
2019-10-13 14:58:28 +0100

    change the uri of logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../src/assets/images/logo.svg';
 import './App.css';
 import {Link} from "react-router-dom";
 import { Button, Icon} from "antd";
